2249dc466c5f875a5444770ad2b003c4152e8b5d
dbkinghorn <dbk@mergencecomputing.com>
2020-11-10 18:09:56 -0800

    maxQ plots nb

--- notes/multi-gpu-rtx30/maxQ/maxQ-data.sh
+++ notes/multi-gpu-rtx30/maxQ/maxQ-data.sh
@@ -13,15 +13,15 @@ OUT_FILE=${TEST_DIR}/${GPU_NAME}_maxQ.out
 export ENROOT_MOUNT_HOME=y
 #export NVIDIA_DRIVER_CAPABILITIES=all
 
-CUDA_VISIBLE_DEVICES=0,1
-sudo nvidia-smi -pm 1 -i 0,1
+CUDA_VISIBLE_DEVICES=0,1,2
+sudo nvidia-smi -pm 1 -i 0,1,2
 
 for w in {100..350..10}
 do
 
-sudo nvidia-smi -pl $w -i 0,1
+sudo nvidia-smi -pl $w -i 0,1,2
 #enroot start ngc-tf1-10 python nvidia-examples/cnn/resnet.py  --layers=50  --batch_size=96  --precision=fp32 -i 120 | tee ${OUT_FILE}
-enroot start ngc-tf1-10 mpiexec --allow-run-as-root --bind-to socket -np 2 python nvidia-examples/cnn/resnet.py --layers=50 -b 96 --precision=fp32 -i 180 | tee ${OUT_FILE}
+enroot start ngc-tf1-10 mpiexec --allow-run-as-root --bind-to socket -np 3 python nvidia-examples/cnn/resnet.py --layers=50 -b 96 --precision=fp32 -i 180 | tee ${OUT_FILE}
 
 ips=$(grep -E '^\s+[0-9]+\s+' ${OUT_FILE} | tr -s ' ' | cut -d ' ' -f 4 | sort -n | tail -1 ) 
 
